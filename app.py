c2f505ca26da89a47efbf60d4ea77205e73cafe1
Shreyansh Gupta <120119737+shrey7ansh07@users.noreply.github.com>
2024-10-14 11:03:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,4 @@
 import os
-__import__('pysqlite3')
-import sys
-sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 import sys
 from langchain import HuggingFaceHub
 from langchain.text_splitter import CharacterTextSplitter
